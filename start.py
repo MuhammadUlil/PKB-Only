7e243c5604b3e1151e75eecdb461f9b673399d77
MuhammadUlil <62757773+MuhammadUlil@users.noreply.github.com>
2020-04-20 20:29:48 +0700

    Update start.py

--- start.py
+++ start.py
@@ -1,8 +1,8 @@
-from mnist import MNIST
 import numpy as np
 from lib import *
+from mnist.loader import MNIST
 
-mndata = MNIST('.data')
+mndata = MNIST('./data')
 images, labels = mndata.load_training()
 t_images, t_labels = mndata.load_testing()
 
@@ -11,7 +11,7 @@ t_images = np.array(t_images)
 labels = np.array(labels)
 t_labels = np.array(t_labels)
 
-print_statistics(images, t_images, labels, t_labels)
+#print_statistics(images, t_images, labels, t_labels)
 
 (m,n) = images.shape
 bias = np.ones((m,1))
@@ -19,5 +19,11 @@ images = np.hstack((bias,images))
 (m2,n2) = t_images.shape
 t_bias = np.ones((m2,1))
 t_images = np.hstack((t_bias,t_images))
+images=np.array([images])
+num_class = len(np.unique(labels))
+theta = np.random.rand(num_class,n+1)
+theta = np.array([theta])
 
-theta = np.random.rand(m,n-1)
+h_final = hypothesis(images,labels)
+print(h_final.shape)
+print(h_final)
